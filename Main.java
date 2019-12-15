2c5beff579da38375caa9811c09d73c4c149816d
JohnEricsonTiu24 <58711705+JohnEricsonTiu24@users.noreply.github.com>
2019-12-15 16:48:12 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,7 +1,16 @@
-public class Main {
-
-  public static void main(String[] args) {
-    System.out.println("Hello World");
-  }
-
+    public class Main {
+   
+    public static void main(String[] args) {
+     
+     Taxi x = new Taxi("Piattos 123");
+     x.run("rudu");
+     x.printCurrentLoc();
+     x.setBook(1,1);
+     x.run("I am leaving here ");
+     x.reachedDestination();
+     System.out.println("Your PlateNumber is " + x.name);
+     System.out.println(x.reachedDestination());
+     
+    }
 }
+
